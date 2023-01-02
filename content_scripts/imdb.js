9af295153339d1bdd2921da927b0e91622b54bae
hypertensiune <81163129+hypertensiune@users.noreply.github.com>
2023-01-02 23:01:12 +0200

    Add files via upload

--- content_scripts/imdb.js
+++ content_scripts/imdb.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", function(){
-    let jsondata = JSON.parse(document.querySelector("script[type='application/ld+json']").innerHTML);
-    chrome.runtime.sendMessage({action: "fetchedPoster", url: jsondata["image"]});
+    let url = document.querySelector("div.ipc-media.ipc-media--poster-27x40.ipc-image-media-ratio--poster-27x40.ipc-media--baseAlt.ipc-media--poster-l.ipc-poster__poster-image.ipc-media__img img").getAttribute("src");
+    chrome.runtime.sendMessage({action: "fetchedPoster", url: url});
     window.close();
 });
