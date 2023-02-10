72d347fa4e9dd11ffeab9d3ac041a08432de008d
hypertensiune <81163129+hypertensiune@users.noreply.github.com>
2023-02-10 16:06:43 +0200

    IMDB update

--- content_scripts/imdb.js
+++ content_scripts/imdb.js
@@ -1,7 +1,7 @@
 if(window.location.search == "?moviewatchlistextension"){
     window.addEventListener("load", function(){
         let url = document.querySelector("div.ipc-media.ipc-media--poster-27x40.ipc-image-media-ratio--poster-27x40.ipc-media--baseAlt.ipc-media--poster-l.ipc-poster__poster-image.ipc-media__img img").getAttribute("src");
-        let date = document.querySelector("span.sc-8c396aa2-2.itZqyK") || document.querySelector("span.sc-8c396aa2-2");
+        let date = document.querySelector("span.sc-f26752fb-2.eqUJdo");
         if(date != null)
             date = date.innerText;
         
