85207bfc4e33da28d461e01cf251ef45803d0e8f
hypertensiune <81163129+hypertensiune@users.noreply.github.com>
2023-06-28 19:12:36 +0300

    Add files via upload

--- content_scripts/content.js
+++ content_scripts/content.js
@@ -1,7 +1,7 @@
 let contentScript = new ContentScript(
     ["div.TzHB6b.Hwkikb.WY0eLb:has(div[data-attrid='kc:/film/film:media_actions_wholepage'])",
     "div.TzHB6b.Hwkikb.WY0eLb:has(div[data-attrid='kc:/tv/tv_program:media_actions_wholepage'])"],
-    ["div.PZPZlf.ssJ7i.B5dxMb[data-attrid=title]"],
+    ["div.PZPZlf.ssJ7i.B5dxMb[data-attrid=title]", "div.PZPZlf.ssJ7i.xgAzOe[data-attrid=title]"],
     ["span.LrzXr.kno-fv.wHYlTd.z8gr9e"],
     ["div[jsname=xQjRM]"],
     "quickActionsGoogle.html"
